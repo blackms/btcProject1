ea88f582d68c3db98a3c5324758e12bd4a76d1bc
Alessio Rocchi <rocchi.b.a@gmail.com>
2017-12-29 23:26:52 +0100

    Moved more method in the base class

--- btcProject1/Clients/Exchange.py
+++ btcProject1/Clients/Exchange.py
@@ -1,39 +1,21 @@
-import json
+from abc import abstractmethod
 from asyncio import coroutine
 
-import requests as re
-from abc import ABCMeta, abstractmethod, abstractproperty
-from tornado.httpclient import AsyncHTTPClient, HTTPRequest, TracebackFuture, HTTPResponse
 from tornado.gen import Task
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest, TracebackFuture, HTTPResponse
 from tornado.ioloop import IOLoop
 
 
-class Exchange(metaclass=ABCMeta):
+class Exchange(object):
     def __init__(self):
-        pass
+        self.base_value = None
+        self.tickers = None
+        self.currencies = None
+        self.api = None
 
-    def _request(self):
+    @abstractmethod
+    def _process_response(self, resp):
         raise NotImplementedError
-
-    def get_tickers(self):
-        raise NotImplementedError
-
-    @coroutine
-    def sleep(self):
-        yield Task(IOLoop.instance().add_timeout,
-                   IOLoop.instance().time() + 5)
-
-
-class BitStamp(Exchange, object):
-    def __init__(self):
-        super().__init__()
-        self.api = 'https://www.bitstamp.net/api'
-        self.tickers = '/v2/ticker/'
-        self.currencies = ['eth', 'btc', 'xrp', 'bch']
-        self.base_value = 'usd'
-
-    def _process_response(self, resp: HTTPResponse):
-        print(resp.body)
 
     def _request(self, path: str=None, method: str=None, body: str=None) -> TracebackFuture:
         client = AsyncHTTPClient()
@@ -53,10 +35,27 @@ class BitStamp(Exchange, object):
         return self._request(path=path, method='GET')
 
     @coroutine
+    def sleep(self):
+        yield Task(IOLoop.instance().add_timeout,
+                   IOLoop.instance().time() + 5)
+
+    @coroutine
     def run(self):
         for currency in self.currencies:
             yield self.get_tickers(pair=currency)
         yield self.sleep()
+
+
+class BitStamp(Exchange):
+    def __init__(self):
+        super().__init__()
+        self.api = 'https://www.bitstamp.net/api'
+        self.tickers = 'v2/ticker/'
+        self.currencies = ['eth', 'btc', 'xrp', 'bch']
+        self.base_value = 'usd'
+
+    def _process_response(self, resp: HTTPResponse):
+        print(resp.body)
 
 
 class BitFinex(Exchange, object):
@@ -69,29 +68,6 @@ class BitFinex(Exchange, object):
 
     def _process_response(self, resp: HTTPResponse):
         print(resp.body)
-
-    def _request(self, path: str=None, method: str=None, body: str=None) -> TracebackFuture:
-        client = AsyncHTTPClient()
-        request = HTTPRequest(
-            url=self.api + path,
-            method=method,
-            body=body,
-        )
-        return client.fetch(
-            request=request,
-            callback=lambda resp: self._process_response(resp)
-        )
-
-    @coroutine
-    def get_tickers(self, pair: str='btcusd'):
-        path = "{}{}{}".format(self.tickers, pair, self.base_value)
-        return self._request(path=path, method='GET')
-
-    @coroutine
-    def run(self):
-        for currency in self.currencies:
-            yield self.get_tickers(pair=currency)
-        yield self.sleep()
 
 
 @coroutine
